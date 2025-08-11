89acb04203899c91a25d30c3e251593517e7989d
mobinsamadir <118572208+mobinsamadir@users.noreply.github.com>
2025-08-12 02:24:13 +0330

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,9 +1,10 @@
+// File: android/build.gradle.kts
+
 plugins {
-    id("com.android.application") version "8.2.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
 }
-
-
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
